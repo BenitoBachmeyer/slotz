27753697c72d2942a18e5361af11b62396ec9b58
BenitoBachmeyer <benitobachmeyer@gmail.com>
2026-03-08 07:55:07 +0100

    added health check

---
+++ backend1/src/main/java/hwr/backend1/health/HealthController.java
@@ -0,0 +1,17 @@
+package hwr.backend1.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok("Healthy Backend");
+    }
+}
